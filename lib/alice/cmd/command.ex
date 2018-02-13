f92e637a0fa7df02a99f161f9062a34e104c5307
amy null <null@amy.gg>
2018-02-12 23:23:40 -0500

    Slightly more useful logging

--- lib/alice/cmd/command.ex
+++ lib/alice/cmd/command.ex
@@ -38,74 +38,89 @@ defmodule Alice.Command do
 
     # TODO: Remove this, testing-only~
     if ctx["author"]["id"] == 128316294742147072 do
-      words = String.split msg, ~R/\s+/, [parts: 2, trim: true]
-      cmd = words |> List.first
-      channel = Alice.Cache.get_channel ctx["channel_id"]
-      lang = Alice.Database.get_language channel["guild_id"]
-      unless is_nil cmd do
-        custom_prefix = Alice.Database.get_custom_prefix channel["guild_id"]
+      try do
+        words = String.split msg, ~R/\s+/, [parts: 2, trim: true]
+        cmd = words |> List.first
+        channel = Alice.Cache.get_channel ctx["channel_id"]
+        lang = Alice.Database.get_language channel["guild_id"]
+        unless is_nil cmd do
+          custom_prefix = Alice.Database.get_custom_prefix channel["guild_id"]
 
-        {prefixed, prefix} = 
-            if String.starts_with?(cmd, get_prefix()) do
-              {true, get_prefix()}
-            else
-              unless is_nil custom_prefix do
-                if String.starts_with?(cmd, custom_prefix) do
-                  {true, custom_prefix}
+          {prefixed, prefix} = 
+              if String.starts_with?(cmd, get_prefix()) do
+                {true, get_prefix()}
+              else
+                unless is_nil custom_prefix do
+                  if String.starts_with?(cmd, custom_prefix) do
+                    {true, custom_prefix}
+                  else
+                    {false, nil}
+                  end
                 else
                   {false, nil}
                 end
-              else
-                {false, nil}
               end
+
+          if prefixed do
+            cmd_name = cmd |> String.slice(String.length(prefix)..2048)
+            argstr = if length(words) > 1 do
+              words |> List.last
+            else
+              ""
+            end
+            args = if length(words) > 1 do
+              words |> List.last |> String.split(~R/\s+/, [trim: true])
+            else
+              []
             end
 
-        if prefixed do
-          cmd_name = cmd |> String.slice(String.length(prefix)..2048)
-          argstr = if length(words) > 1 do
-            words |> List.last
-          else
-            ""
-          end
-          args = if length(words) > 1 do
-            words |> List.last |> String.split(~R/\s+/, [trim: true])
-          else
-            []
-          end
-
-          unless is_ratelimited? ctx["author"] do
-            match = Alice.CommandState.get_command cmd_name
-            unless is_nil match do
-              invoke = match[:invoke]
-              unless is_nil invoke do
-                try do
-                  invoke.(cmd_name, args, argstr, ctx)
-                rescue
-                  e -> 
-                    # TODO: Make this log into Sentry instead
-                    err = ctx
-                          |> error(
-                            """
-                            ```Elixir
-                            #{Exception.format(:error, e)}
-                            ```
-                            """
-                            )
-                    Emily.create_message ctx["channel_id"], [content: nil, embed: err]
+            unless is_ratelimited? ctx["author"] do
+              match = Alice.CommandState.get_command cmd_name
+              unless is_nil match do
+                invoke = match[:invoke]
+                unless is_nil invoke do
+                  try do
+                    invoke.(cmd_name, args, argstr, ctx)
+                  rescue
+                    e -> 
+                      # TODO: Make this log into Sentry instead
+                      err = ctx
+                            |> error(
+                              """
+                              ```Elixir
+                              #{Exception.format(:error, e)}
+                              ```
+                              """
+                              )
+                      Emily.create_message ctx["channel_id"], [content: nil, embed: err]
+                  end
+                else
+                  Logger.warn "Caught nil invoke for #{cmd_name}!"
                 end
-              else
-                Logger.warn "Caught nil invoke for #{cmd_name}!"
               end
+            else
+              ctx
+              |> error(Alice.I18n.translate(lang, "message.ratelimited"))
+              |> Emily.create_message(ctx["channel_id"])
             end
           else
-            ctx
-            |> error(Alice.I18n.translate(lang, "message.ratelimited"))
-            |> Emily.create_message(ctx["channel_id"])
+            # Not a command, send it elsewhere
+            Alice.LevelsHandler.process_message ctx
           end
-        else
-          # Not a command, send it elsewhere
-          Alice.LevelsHandler.process_message ctx
         end
+      rescue
+        e ->
+          if ctx["author"]["id"] == 128316294742147072 do
+            err = ctx
+                  |> error(
+                    """
+                    ```Elixir
+                    #{Exception.format(:error, e)}
+                    ```
+                    """
+                    )
+            Emily.create_message ctx["channel_id"], [content: nil, embed: err]
+          end
       end
     end
   end
